7fd137ba6eaf9f7d918b9dd0c22b47891ef43911
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2021-01-25 12:30:52 +0545

    Setup build and bin

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 export default {
   input: {
-    cli: 'index.js',
+    index: 'index.js',
     git: 'git.js',
     dir: 'dir.js'
   },
   output: {
-    dir: 'dist',
+    dir: 'lib',
     format: 'cjs',
     chunkFileNames: '[name].js',
     banner: '#! /usr/bin/env node\n',
